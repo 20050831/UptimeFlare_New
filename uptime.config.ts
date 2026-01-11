36b00a278d8a64d258f4ed77dadb6a4bf88ffc38
George <lobsterdreams@163.com>
2026-01-11 12:43:24 -0500

    Update status page title and links

--- uptime.config.ts
+++ uptime.config.ts
@@ -7,12 +7,11 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Lobster Studio's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://longxiastudio.com', label: 'Lobster Blog' },
+    { link: 'mailto:lobsterx@duck.com', label: 'Email Me', highlight: true },
   ],
 }
 
